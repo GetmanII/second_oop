67a0ac2a28445bc8b01f0c564249dff1726dec33
GetmanII <getmanovii@mail.ru>
2025-01-17 03:32:56 +0200

    lesson_10

--- classes/user.py
+++ classes/user.py
@@ -1,75 +1,49 @@
-import bcrypt
+class UserAlreadyExistsError(Exception):
+    def __init__(self, username: str, message: str = "Пользователь с таким именем уже существует"):
+        self.message = message
+        self.username = username
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return f" {self.message}: {self.username}"
 
+
+class UserNotFoundError(Exception):
+    def __init__(self, username: str, message: str = "Пользователь не найден"):
+        self.message = message
+        self.username = username
+        super().__init__(self.message)
+    
+    def __str__(self):
+        return f" {self.message}: {self.username}"
 
 class User:
-    users = [] 
-    def __init__(self, username, email, password):
+    def __init__(self, username: str, email: str, age: int):
         self.username = username
         self.email = email
-        self.password_hash = self.hash_password(password)
-        User.users.append(self)
-    def hash_password(self, password):
-        salt = bcrypt.gensalt()
-        return bcrypt.hashpw(password.encode(), salt)
-    
-    def check_password(self, password):
-        return bcrypt.checkpw(password.encode(), self.password_hash)
-    
-class Customer(User):
-    def __init__(self, username, email, password, address):
-        super().__init__(username, email, password)
-        self.address = address
-    
-    def get_details(self):
-        return f"Клиент: {self.username}, Email: {self.email}, Адрес: {self.address}"
-    
-class Admin(User):
-    """
-    Класс, представляющий администратора, наследующий класс User.
-    """
-    def __init__(self, username, email,  password, admin_level):
-        super().__init__(username, email, password)
-        self.admin_level = admin_level
+        self.age = age
 
-    def get_details(self):
-        return f"Admin: {self.username}, Email: {self.email}, Admin-Level: {self.admin_level}"
+    def __str__(self):
+        return f"User: {self.username}, Email: {self.email}, Age: {self.age}"
     
-    @staticmethod
-    def list_users():
-        return [user.get_details() for user in User.users]
-    
-    @staticmethod
-    def delete_user(username):
-        for user in User.users:
-            if user.username == username:
-                User.users.remove(user)
-                return f"Пользователь {username} удален."
-        return f"Пользователь {username} не найден."
-            
-class AuthenticationService:
-    def __init__(self):
-        self.current_user = None
+
+class UserManager:
+    users = {}
         
-    def register(self, user_class, username, email, password, *args):
-        if any(user.username == username for user in User.users):
-            return f"Пользователь {username} с таким именем уже зарегистрирован."
-        user_class(username, email, password, *args)
-        return f"Пользователь {username} зарегистрирован."
+    def add_user(self, user: User):
+        if user.username in UserManager.users:
+            raise UserAlreadyExistsError(user.username)
+        UserManager.users[user.username] = user
+        return f'Пользователь {user.username} добавлен'
     
-    def login(self, username, password):
-        for user in User.users:
-            if user.username == username and user.check_password(password):
-                self.current_user = user
-                return f"Пользователь {username} успешно авторизован."
-        return f"Неверное имя пользователя или пароль."
-    
-    def logout(self):
-        if self.current_user:
-            logged_out_user = self.current_user.username
-            self.current_user = None
-            return f"Пользователь {logged_out_user} успешно вышел из системы."
-        return "Никто не авторизован."
-    def get_current_user(self):
-        if self.current_user:
-            return f"Пользователь в сети: {self.current_user.get_details()}"
-        return "Никто не авторизован."
+
+    def remove_user(self, username: str):
+        if username not in UserManager.users:
+            raise UserNotFoundError(username)
+        del UserManager.users[username]
+        return f'Пользователь {username} удален'
+        
+    def find_user(self, username: str):
+        if username not in UserManager.users:
+            raise UserNotFoundError(username)
+        return UserManager.users[username]
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from classes.user import User, Customer, Admin, AuthenticationService
+from classes.user import User, UserManager
 
+manager = UserManager()
 
-auth_service = AuthenticationService()
-print(auth_service.register(Customer, "Антон", "anton@example.com", "password123", "Улица Пушкина, дом Колотушкина"))
-print(auth_service.register(Admin, "admin", "admin@example.com", "adminpass", 1))
-print(auth_service.login("Антон", "password1234"))
-print(auth_service.login("Антон", "password123"))
-print(auth_service.get_current_user())
-print(auth_service.logout())
-print(Admin.list_users())
-print(Admin.delete_user("Антон"))
-print(Admin.list_users())
+print(manager.add_user(User("user1", "email1", 20)))
+print(manager.add_user(User("user2", "email2", 30)))
+print(manager.add_user(User("user1", "email3", 40)))
+
+print(manager.find_user("user1"))
+
+print(manager.remove_user("user1"))
+
+print(manager.remove_user("user1"))
+
+print(manager.find_user("user1"))
