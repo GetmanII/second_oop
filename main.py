819570f239182c640887acb3a32aba9056b6510b
GetmanII <getmanovii@mail.ru>
2025-01-17 03:40:02 +0200

    lesson_10

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ manager = UserManager()
 
 print(manager.add_user(User("user1", "email1", 20)))
 print(manager.add_user(User("user2", "email2", 30)))
-print(manager.add_user(User("user1", "email3", 40)))
+# print(manager.add_user(User("user1", "email3", 40)))
 
 print(manager.find_user("user1"))
 
 print(manager.remove_user("user1"))
 
-print(manager.remove_user("user1"))
+# print(manager.remove_user("user1"))
 
 print(manager.find_user("user1"))
